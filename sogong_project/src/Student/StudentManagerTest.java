13e9d7da9d82ead776199dd4b187e5e5d5329e25
rlaqufl0711 <rlaqufl711@g.hongik.ac.kr>
2025-05-26 19:53:05 +0900

    manager commit

---
+++ sogong_project/src/Student/StudentManagerTest.java
@@ -0,0 +1,42 @@
+package Student;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StudentManagerTest {
+	
+	StudentManager manager;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		manager = new StudentManager();
+	}
+
+	
+	@Test
+	void testAddStudent() {
+		manager.addStudent("Kim");
+		assertTrue(manager.hasStudent("Kim"));
+	}
+
+	@Test
+	void testRemoveStudent() {
+		manager.addStudent("Park");
+		manager.removeStudent("Park");
+		assertFalse(manager.hasStudent("Park"));
+	}
+
+	@Test
+	void testAddDupStudent() {
+		manager.addStudent("Jung");
+		assertThrows(IllegalArgumentException.class, () -> manager.addStudent("Jung"));
+	}
+	
+	@Test
+	void testRemoveNonexistStudent() {
+		assertThrows(IllegalArgumentException.class, () -> manager.removeStudent("Lee"));
+	}
+
+}
